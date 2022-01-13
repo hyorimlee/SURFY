1aaf8461b204c50df6c3573e795fec1341d5edfb
홍종규 <kongi4289@naver.com>
2022-01-13 06:10:28 +0000

    Update index.jsx

--- frontend/src/layout/Header/index.jsx
+++ frontend/src/layout/Header/index.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
-import Weather from '../../components/Weather/';
-import BusStationName from '../../components/Bus/BusStationName';
-import CardComponent from '../../components/Box';
-=======
-
->>>>>>> a56dbeb3ba37dc22f20941ed21d3bc8db90fd179
 import Wrapper from './styles';
 
 const Header = (props) => {
   return (
     <Wrapper>
-<<<<<<< HEAD
-      <Weather />
-      <BusStationName />
-      <CardComponent />
-    </Wrapper>
-  )
-=======
       Header
     </Wrapper>
   );
->>>>>>> a56dbeb3ba37dc22f20941ed21d3bc8db90fd179
 }
 
 export default Header;
