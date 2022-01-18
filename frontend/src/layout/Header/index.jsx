ce4f72bd4e62aca3fcaa263f00676feb369588a7
unho-00 <djsgh94@naver.com>
2022-01-18 15:23:40 +0900

    feat 웹 레이아웃 구성

--- frontend/src/layout/Header/index.jsx
+++ frontend/src/layout/Header/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+
 import Wrapper from './styles';
 
 
 const Header = (props) => {
   return (
     <Wrapper>
-      Header
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <p>Logo</p>
+        <p>Account</p>
+      </Grid>
     </Wrapper>
   );
 }
--- frontend/src/layout/Header/styles.jsx
+++ frontend/src/layout/Header/styles.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  backgroundColor: blue;
+  widthL 100%;
 `;
 
 export default Wrapper;
