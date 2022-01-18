eec4fb8de1a3298e4ce60121b2aa8245e79f1b53
unho-00 <djsgh94@naver.com>
2022-01-18 16:35:30 +0900

    feat 웹 페이지 네비게이션 로그인 로그아웃 버튼 추가

--- frontend/src/layout/Header/index.jsx
+++ frontend/src/layout/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
 
 import Wrapper from './styles';
 
-
 const Header = (props) => {
+  const { isLogin } = props;
+
   return (
     <Wrapper>
       <Grid
@@ -14,7 +16,21 @@ const Header = (props) => {
         alignItems="center"
       >
         <p>Logo</p>
-        <p>Account</p>
+        {
+          isLogin
+          ? <p>Account</p>
+          : (
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-end"
+              alignItems="center"
+            >
+              <Link to="signin">로그인</Link>
+              <Link to="signup">회원가입</Link>
+            </Grid>
+          )
+        }
       </Grid>
     </Wrapper>
   );
