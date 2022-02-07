1baaf7caef1a3a259f28abbc71c6c8cdc3129cc6
hyorimlee <hl1046@pusan.ac.kr>
2022-02-07 14:09:41 +0900

    feat: add routers for member API

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ const surveyRouter = require('./survey')
 const businfoRouter = require('./businfo');
 const weatherRouter = require('./weather');
 const rouletteRouter = require('./roulette');
+const memberRouter = require('./member');
 
 const router = express.Router();
 
@@ -10,4 +11,5 @@ router.use('/api/survey',surveyRouter)
 router.use("/api/businfo", businfoRouter);
 router.use("/api/weather", weatherRouter);
 router.use("/api/roulette",rouletteRouter);
+router.use("/api/member",memberRouter)
 module.exports = router;
