2f8b2ca4fa3890bc905f2e0cc3297f2776402152
mage888 <mage888@naver.com>
2022-02-08 15:27:53 +0900

    feat 마일리지 적립/환급 페이지

--- frontend/src/pages/Web/index.jsx
+++ frontend/src/pages/Web/index.jsx
@@ -3,6 +3,7 @@ import { Grid } from '@material-ui/core';
 
 // import NowSurvey from './NowSurvey/index';
 import OtherSurvey from './OtherSurvey/index';
+import MileageSave from './MileageSave/index';
 import Login from '../../components/Auth/Login/index';
 
 import Layout from '../../layout/layout';
@@ -20,10 +21,6 @@ const Web = (props) => {
           justifyContent="center"
           alignItems="center"
         >
-          {/* 현재 진행중 설문조사 */}
-          {/* <NowSurvey>
-          </NowSurvey> */}
-          {/* 다른 진행 설문조사 */}
           <OtherSurvey>
           </OtherSurvey>
           <Login></Login>
