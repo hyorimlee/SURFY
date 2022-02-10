dcc069a424541ff51c6d39f4a3d1a769bd767806
JongKyuHong <kongi7064@gmail.com>
2022-02-10 23:36:55 +0900

    fix : 인포윈도우 수정

--- frontend/src/components/Marker/Infowindow.jsx
+++ frontend/src/components/Marker/Infowindow.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import Wrapper from './styles'
 const InfoWindow = ({place}) => {
+  
+  const infoWindowStyle = {
+    position: "relative",
+    bottom: 50,
+    left: "20px",
+    width: 100,
+    backgroundColor: "white",
+    boxShadow: "0 2px 7px 1px rgba(0, 0, 0, 0.3)",
+    padding: 10,
+    fontSize: 14,
+    zIndex: 100,
+  };
+
   return (
-    <Wrapper >
-      <div className="infowindow" style={{width: 100, height:150}}>
-        {place.text}
+    <Wrapper>
+      <div style={infoWindowStyle}>
+        <div style={{fontSize: 12}} >{place.text}</div>
         <hr/>
-        {place.op_time}
+        <div style={{fontSize: 10}} >{place.op_time}</div>
       </div>
       {/* <textarea value={place.text + '\n' + place.op_time} cols="30" rows="10">
       </textarea> */}
     </Wrapper>
-  
   )
 }
 export default InfoWindow;
