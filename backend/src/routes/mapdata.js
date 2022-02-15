5fc8f56ed01329a6cbd012ddbb8605dce80edf97
JongKyuHong <kongi7064@gmail.com>
2022-02-15 15:27:43 +0900

    fix: mapMarker

--- backend/src/routes/mapdata.js
+++ backend/src/routes/mapdata.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const app = express.Router()
-const ctrl = require('../controller/ctrl')
 
-app.get('/',ctrl.output.mapdata)
+const fs = require('fs');
+const {development} = require('../config/config.json')
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: development.host,
+    user: development.username,
+    password : development.password,
+    port : 3306,
+    database: development.database
+})
+connection.connect();
+
+app.get('/', async(req, res)=>{
+  connection.query(
+    "SELECT * FROM maptest", (err, data) =>{
+        if(!err){
+            res.send(data);
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+  });
+})
 
 module.exports = app
