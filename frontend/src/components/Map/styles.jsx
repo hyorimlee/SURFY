43093579f63590d7c2415b564be50ebadc0dd27b
mage888 <mage888@naver.com>
2022-02-17 21:13:08 +0900

    fix: 키오스크 UI 크기 임시수정2

--- frontend/src/components/Map/styles.jsx
+++ frontend/src/components/Map/styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
 
-  width: 325px;
+  width: 500px;
   height: 176px;
 
 `;
--- frontend/src/components/Time/styles.jsx
+++ frontend/src/components/Time/styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   font-family: 'Roboto', sans-serif;
-  font-size: 0.7rem;
+  font-size: 0.8rem;
 `;
 
 export default Wrapper;
