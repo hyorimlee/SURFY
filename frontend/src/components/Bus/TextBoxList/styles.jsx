53316d34c612cfd4d4a0b285ce7ca252e2ddc1cc
mage888 <mage888@naver.com>
2022-02-18 10:34:21 +0900

    fix: 키오스크 UI 크기 임시수정3

--- frontend/src/components/Bus/TextBoxList/styles.jsx
+++ frontend/src/components/Bus/TextBoxList/styles.jsx
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 import { Grid } from '@material-ui/core';
 
 const Wrapper = styled.div`
-  width: 40px;
+  width: 280px !important;
   color: black;
-  padding: 30px;
+  padding: 5px;
 `;
 
 const CustomGrid = styled(Grid)`
-  padding: 7px !important;
+  padding: 2px !important;
   text-align: center !important;
   font-size: ${ props => props.children.length >= 5 ? "1rem" : "1.2rem" };
 `;
--- frontend/src/pages/Kiosk/BusSoon/styles.jsx
+++ frontend/src/pages/Kiosk/BusSoon/styles.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   font-family: 'Roboto';
   width: 98%;
-  height: 30px;
+  height: 60px;
   
   background-color: #456173;
   border-top-left-radius: 10px;
@@ -21,7 +21,7 @@ const Wrapper = styled.div`
   }
 
   & .bus-soon--list {
-    width: 180px;
+    width: 350px;
     height: 80%;
     margin: 0 10px;
     border-radius: 10px;
